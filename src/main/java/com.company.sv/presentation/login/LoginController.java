20af42cf1ec2e1f815d35da8717984446144e8cd
darrenmark <darrenmark1@yahoo.com>
2012-09-13 10:23:00 -0700

    Initial setup

--- src/main/java/com.company.sv/presentation/login/LoginController.java
+++ src/main/java/com.company.sv/presentation/login/LoginController.java
@@ -1,6 +1,7 @@
 package com.company.sv.presentation.login;
 
 import com.company.sv.domain.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +12,19 @@ import javax.servlet.http.HttpSession;
 
 /**
  */
+@Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    @RequestMapping("/login.login")
     public String login(ModelMap model, @ModelAttribute("user") User user, HttpSession session, HttpServletRequest request) {
         System.out.println("Callled...");
         if (request.getMethod().equals("POST") ) {
             if(user.getLoginId().equals("darren")) {
-                return "forward:./main";
+                return "forward:./main.jsp";
             }
         } else {
             model.put("user", new User());
         }
-        return "forward:./login";
+        return "forward:./login.jsp";
     }
 }
